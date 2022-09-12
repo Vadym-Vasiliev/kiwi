2ced22c498c0ff3b2ebd82cab15867bb2c145ca8
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-09-12 12:44:39 +0300

    add fix blog slider

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -2,4 +2,7 @@ import $ from "jquery";
 import "slick-carousel";
 
 $(".reviews-bottom").slick();
-$(".blog-list").slick();
+$(".blog-list").slick({
+  variableWidth: true,
+  centerMode: true,
+});
